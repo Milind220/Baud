f9b611471822d14e2290ae6c18132738e63a78c7
Milind Sharma <milindsharma8@gmail.com>
2024-04-27 17:33:59 +0530

    Add TODO comments

--- baud-cli/src/commands.rs
+++ baud-cli/src/commands.rs
@@ -3,6 +3,16 @@ use baud_core::serial::{PortType, list_available_ports};
 use baud_core::connection::SerialConnection;
 use tokio::runtime;
 
+// TODO: 1. Make list_ports differentiate between ports that refer to the same physical port. Don't
+// repeat unless the user specifies the --all flag.
+// TODO: 2. Test writing to serial port and see how to make it better.
+// TODO: 3. Convert data bytes to decimal format and display it in the output without any
+// additional text.
+// TODO: 4. Add a header with colour when a serial port connection is opened.
+// TODO: 5. Add a hacker mode that displays matrix rain when a serial port connection is opened.
+// TODO: 6. Add a work mode that displays random data to impreess the boss
+
+/// Handles the command line arguments and executes the appropriate command.
 pub fn handle_matches(matches: ArgMatches) {
     match matches.subcommand() {
         Some(("list", _sub_matches)) => list_ports(),
@@ -12,6 +22,7 @@ pub fn handle_matches(matches: ArgMatches) {
     }
 }
 
+/// Opens a serial port and reads data from it.
 fn open_port(sub_matches: &ArgMatches) {
     let port_name = sub_matches.get_one::<String>("port").expect("Port name is required");
     let baud_rate: u32 = sub_matches.get_one::<String>("baud")
@@ -31,6 +42,7 @@ fn open_port(sub_matches: &ArgMatches) {
     });
 }
 
+/// Lists available serial ports.
 fn list_ports() {
     let ports = list_available_ports().unwrap();
     for port in ports {
